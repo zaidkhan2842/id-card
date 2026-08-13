abe944f2cfbb181ce39cd6baf515cd863079a929
zaidkhan2842 <zk392061@gmail.com>
2026-08-13 20:20:20 +0530

    Fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/id-card",
   server: {
     port: 5173
   },
